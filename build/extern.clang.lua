cc43db8d22ddbbd51961582a87915f5cef5635e1
Eric Parayre <eric.parayre@gmail.com>
2016-03-18 00:26:43 -0400

    appveyor * download llvm-3.8.0 to the externs * enabled libclang on windows, based on its path in the externs

--- build/extern.clang.lua
+++ build/extern.clang.lua
@@ -111,8 +111,9 @@ function AddExternClangLib()
     elseif os.get() == "linux" then
         libclang = ":libclang-3.7.so.1"
         libclangPath = "/usr/lib/llvm-3.7/lib"
-    else
-        return
+    elseif os.get() == "windows" then
+        libclang = "libclang"
+        libclangPath = "../externs/llvm-3.8.0/lib"
     end
 
     defines { "EXTERN_CLANG_ENABLED" }
@@ -127,5 +128,9 @@ function AddExternClangLib()
 
     links { libclang }
 
-    linkoptions { "-Xlinker -rpath " .. libclangPath }
+    if os.get() == "windows" then
+        prebuildcommands { "copy " .. path.translate("../../externs/llvm-3.8.0/bin/libclang.dll") .. " $(TargetDir)" }
+    else
+        linkoptions { "-Xlinker -rpath " .. libclangPath }
+    end
 end
