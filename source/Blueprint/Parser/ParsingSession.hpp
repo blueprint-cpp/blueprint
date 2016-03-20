079f75da6a39b1a57003cd1bb1adb40df6a74ed0
Eric Parayre <eric.parayre@gmail.com>
2016-03-20 14:34:26 -0400

    added a ParsingSession class

---
+++ source/Blueprint/Parser/ParsingSession.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#if defined(EXTERN_CLANG_ENABLED)
+
+#include <filesystem/path.h>
+
+namespace blueprint
+{
+    class ParsingSession
+    {
+    public:
+        void SetWorkspace(const filesystem::path& workspace);
+        const filesystem::path& GetWorkspace() const;
+
+        void SetOutputDirectory(const filesystem::path& outputDirectory);
+        const filesystem::path& GetOutputDirectory() const;
+
+        void AddArgument(const std::string& argument);
+        const std::vector<std::string>& GetArguments() const;
+
+    private:
+        std::vector<std::string> arguments_;
+
+        filesystem::path workspace_;
+        filesystem::path outputDirectory_;
+    };
+}
+
+#endif
---
+++ test/unit/Blueprint.Test/Parser/TestParsingSession.cpp
@@ -0,0 +1,51 @@
+#include <catch/catch.hpp>
+
+#if defined(EXTERN_CLANG_ENABLED)
+
+#include "Blueprint/Parser/ParsingSession.hpp"
+#include "TestHelpers/FileSystem.hpp"
+
+TEST_CASE("TestParsingSession")
+{
+    blueprint::ParsingSession session;
+
+    SECTION("Default State")
+    {
+        CHECK(session.GetWorkspace().empty());
+        CHECK(session.GetOutputDirectory().empty());
+        CHECK(session.GetArguments().empty());
+    }
+
+    SECTION("Workspace")
+    {
+        session.SetWorkspace("some/file");
+        CHECK(NormalizedPath(session.GetWorkspace()) == "some/file");
+
+        session.SetWorkspace("some/other/file");
+        CHECK(NormalizedPath(session.GetWorkspace()) == "some/other/file");
+    }
+
+    SECTION("Output Directory")
+    {
+        session.SetOutputDirectory("some/directory");
+        CHECK(NormalizedPath(session.GetOutputDirectory()) == "some/directory");
+
+        session.SetOutputDirectory("some/other/directory");
+        CHECK(NormalizedPath(session.GetOutputDirectory()) == "some/other/directory");
+    }
+
+    SECTION("Argument")
+    {
+        session.AddArgument("arg_A");
+
+        REQUIRE(session.GetArguments().size() == 1);
+        CHECK(session.GetArguments()[0] == "arg_A");
+
+        session.AddArgument("arg_B");
+
+        REQUIRE(session.GetArguments().size() == 2);
+        CHECK(session.GetArguments()[1] == "arg_B");
+    }
+}
+
+#endif
