a297c0a31c24bce097471409f524b94edbf9cab4
Eric Parayre <eric.parayre@gmail.com>
2016-04-02 19:46:42 -0400

    setup-llvm for linux, but only if not running on travis

--- dependencies/setup-llvm.sh
+++ dependencies/setup-llvm.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 if [[ $(uname) == "Darwin" ]]; then
-    llvm=llvm38
 
-    echo brew install $llvm
-    brew install $llvm
+    packages="llvm38"
+
+    echo brew install $packages
+    brew install $packages
     echo -en "\n"
+
+elif [[ -z ${TRAVIS} ]]; then
+
+    packages="clang-3.8 libclang-3.8-dev"
+
+    echo apt-get install $packages
+    sudo apt-get -yq --force-yes install $packages
+    echo -en "\n"
+
 fi
--- dependencies/setup.sh
+++ dependencies/setup.sh
@@ -3,9 +3,21 @@
 pushd $(dirname $0) > /dev/null
 
 if [[ $(uname) == "Darwin" ]]; then
+
     echo brew update
     brew update
     echo -en "\n"
+
+elif [[ -z ${TRAVIS} ]]; then
+
+    echo "deb http://llvm.org/apt/wily/ llvm-toolchain-wily-3.8 main"     | sudo tee    /etc/apt/sources.list.d/llvm.list
+    echo "deb-src http://llvm.org/apt/wily/ llvm-toolchain-wily-3.8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo -en "\n"
+
+    echo apt-get update
+    sudo apt-get update
+    echo -en "\n"
+
 fi
 
 ./setup-premake.sh
