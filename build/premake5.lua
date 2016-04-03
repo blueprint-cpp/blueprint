d2b25598b21ddd2fd94d2bcf31c643d0b324c1fc
Eric Parayre <eric.parayre@gmail.com>
2016-04-03 01:09:50 -0400

    complete rewrite of the premake script

--- build/premake5.lua
+++ build/premake5.lua
@@ -6,48 +6,21 @@ require("extern.json")
 require("extern.philsquared")
 require("extern.sqlite")
 
-function GenerateWorkspace()
-    workspace( "Blueprint" )
-
+workspace("Blueprint")
     platforms { "x64" }
 
     configurations { "Debug", "Release" }
-        location( _ACTION )
+        location(_ACTION)
 
-    configuration( "Debug" )
-        targetdir( "../output/bin/Debug" )
-        objdir( "../output/obj" )
-
-    configuration( "Release" )
-        targetdir( "../output/bin/Release" )
-        objdir( "../output/obj" )
-
-    AddExeProject( "BlueprintApp", "../source/BlueprintApp", "Blueprint", { "Blueprint" } )
-    AddLibProject( "Blueprint", "../source/Blueprint" )
-    AddTestProject( "Blueprint.Test", "../test/unit", { "Blueprint" } )
-end
-
-function AddProject( projectName, sourcePath, projectKind, targetName, projectDependencies )
-    project( projectName )
-        targetname( targetName )
-        kind( projectKind )
-        language( "C++" )
-
-    links(projectDependencies)
-
-    includedirs { "../source" }
-
-    files {
-        path.join( sourcePath, "**.h" ),
-        path.join( sourcePath, "**.hpp" ),
-        path.join( sourcePath, "**.cpp" )
-    }
-
-    configuration { "Debug" }
+    configuration("Debug")
+        targetdir("../output/bin/Debug")
+        objdir("../output/obj")
         defines { "DEBUG" }
         flags { "ExtraWarnings", "FatalWarnings", "Symbols" }
 
-    configuration { "Release" }
+    configuration("Release")
+        targetdir("../output/bin/Release")
+        objdir("../output/obj")
         defines { "NDEBUG" }
         flags { "ExtraWarnings", "FatalWarnings", "Optimize" }
 
@@ -57,38 +30,67 @@ function AddProject( projectName, sourcePath, projectKind, targetName, projectDe
     configuration { "vs*" }
         buildoptions { "/wd4706" }
 
-    AddExternCatch()
+project("BlueprintApp")
+    targetname("Blueprint")
+    kind("ConsoleApp")
+    language("C++")
+
+    links { "Blueprint" }
+
+    includedirs {
+        "../source",
+    }
+
+    files {
+        "../source/BlueprintApp/**.hpp",
+        "../source/BlueprintApp/**.cpp"
+    }
+
+    AddExternClangLib()
     AddExternClara()
+    AddExternFileSystem()
+    AddExternSqlite()
+
+project("Blueprint")
+    kind("StaticLib")
+    language("C++")
+
+    includedirs { "../source" }
+
+    files {
+        "../source/Blueprint/**.hpp",
+        "../source/Blueprint/**.cpp"
+    }
+
     AddExternClangLib()
     AddExternFileSystem()
     AddExternJson()
     AddExternSqlite()
-end
 
-function AddLibProject( projectName, sourcePath )
-    AddProject( projectName, sourcePath, "StaticLib", projectName )
-end
+project("Blueprint.Test")
+    kind("ConsoleApp")
+    language("C++")
 
-function AddExeProject( projectName, sourcePath, targetName, projectDependencies )
-    if targetName then
-        AddProject( projectName, sourcePath, "ConsoleApp", targetName, projectDependencies )
-    else
-        AddProject( projectName, sourcePath, "ConsoleApp", projectName, projectDependencies )
-    end
-end
+    links { "Blueprint" }
 
-function AddTestProject( projectName, sourcePath, projectDependencies )
-    AddExeProject( projectName, sourcePath, projectName, projectDependencies )
+    includedirs {
+        "../source",
+        "../test/unit"
+    }
 
-    includedirs { "../test/unit" }
+    files {
+        "../test/unit/**.hpp",
+        "../test/unit/**.cpp"
+    }
 
     configuration { "gmake" }
         postbuildcommands { "$(TARGET) ../../test/unit/Blueprint.Test" }
 
     configuration { "vs*" }
         postbuildcommands { "\"$(TargetPath)\" ../../test/unit/Blueprint.Test" }
-end
 
-if _ACTION then
-    GenerateWorkspace()
-end
+    AddExternClangLib()
+    AddExternCatch()
+    AddExternFileSystem()
+    AddExternJson()
+    AddExternSqlite()
