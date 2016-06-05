15e093d3a80504b25998ff2b077d46ef2f17d44c
Eric Parayre <eric.parayre@gmail.com>
2016-06-04 21:37:51 -0400

    removed useless change of current directory for unit tests

--- test/unit/Blueprint.Test/Main.cpp
+++ test/unit/Blueprint.Test/Main.cpp
@@ -4,22 +4,8 @@
 #include "BlueprintClang/String.hpp"
 #include "Blueprint/Utilities/WorkingDirectory.hpp"
 
-void ChangeCWD(int argc, const char* argv[])
-{
-    if (argc < 2)
-        return;
-
-    std::string cwd;
-    blueprint::WorkingDirectory::SetCurrent(argv[1]);
-    cwd = blueprint::WorkingDirectory::GetCurrent();
-
-    std::cout << "{ cwd : " << cwd << " }" << std::endl;
-}
-
 int main(int argc, const char* argv[])
 {
-    ChangeCWD(argc, argv);
-
     std::cout << "{ libclang index version " << CINDEX_VERSION_STRING << " }" << std::endl;
     std::cout << "{ libclang " << blueprint::clang::String(clang_getClangVersion()).Get() << " }" << std::endl;
 
