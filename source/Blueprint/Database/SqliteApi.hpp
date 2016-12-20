5db14924e165d585dc8b3ae707a826658bc81031
Eric Parayre <eric.parayre@gmail.com>
2016-12-20 15:50:46 -0500

    sqlite api * disable deprecated declarations warning on clang

--- source/Blueprint/Database/SqliteApi.hpp
+++ source/Blueprint/Database/SqliteApi.hpp
@@ -3,10 +3,15 @@
 #if defined(_MSC_VER)
     #pragma warning(push)
     #pragma warning(disable : 4267)
+#elif defined(__clang__)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
 #include <sqlite3pp/sqlite3pp.h>
 
 #if defined(_MSC_VER)
     #pragma warning(pop)
+#elif defined(__clang__)
+    #pragma clang diagnostic pop
 #endif
